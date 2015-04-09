29a4f5114f1312e029ba7ed25cb5af4c503b2744
Gary W. Smith <gary.w.smith@hp.com>
2015-04-08 17:10:28 -0700

    Incoporate get_item filter

--- manila_ui/dashboards/admin/shares/views.py
+++ manila_ui/dashboards/admin/shares/views.py
@@ -30,6 +30,9 @@ from manila_ui.api import manila
 from manila_ui.dashboards.admin.shares import forms as project_forms
 from manila_ui.dashboards.admin.shares import tabs as project_tabs
 from manila_ui.dashboards.project.shares.shares import views as share_views
+from manila_ui.utils import filters
+
+filters = (filters.get_item,)
 
 
 class IndexView(tabs.TabbedTableView, share_views.ShareTableMixIn):
