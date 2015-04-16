9e0e52b97994e47fd73587628ba91c63cbfd6a6d
Ponomaryov Valeriy <vponomaryov@mirantis.com>
2015-04-16 13:44:15 +0300

    Add nova-network support to share-networks of Manila
    Recently was added support of Nova network to Manila and its API.
    Add its support to UI.
    
    This commit is based on following commits:
    
    581de663295ca05724b5bcfa6ccaf9e20c327081
    71e2f1e3105760a211f0a9f438e6137692eabbd3
    
    From repo https://github.com/NetApp/horizon/tree/manila_kilo

--- manila_ui/dashboards/project/shares/share_networks/tabs.py
+++ manila_ui/dashboards/project/shares/share_networks/tabs.py
@@ -17,31 +17,47 @@ from horizon import exceptions
 from horizon import tabs
 
 from manila_ui.api import manila
+from manila_ui.api import network
 from manila_ui.dashboards.project.shares.share_networks\
     import tables as share_net_tables
 
+from openstack_dashboard.api import base
 from openstack_dashboard.api import neutron
 
 
 class ShareNetworkTab(tabs.TableTab):
-    table_classes = (share_net_tables.ShareNetworkTable,)
     name = _("Share Networks")
     slug = "share_networks_tab"
     template_name = "horizon/common/_detail_table.html"
+
+    def __init__(self, tab_group, request):
+        if base.is_service_enabled(request, 'network'):
+            self.table_classes = (share_net_tables.NeutronShareNetworkTable,)
+        else:
+            self.table_classes = (share_net_tables.NovaShareNetworkTable,)
+        super(ShareNetworkTab, self).__init__(tab_group, request)
 
     def get_share_networks_data(self):
         try:
             share_networks = manila.share_network_list(self.request,
                                                        detailed=True)
-            neutron_net_names = dict([(net.id, net.name) for net in
-                                      neutron.network_list(self.request)])
-            neutron_subnet_names = dict([(net.id, net.name) for net in
-                                        neutron.subnet_list(self.request)])
-            for sn in share_networks:
-                sn.neutron_net = neutron_net_names.get(
-                    sn.neutron_net_id) or sn.neutron_net_id or "-"
-                sn.neutron_subnet = neutron_subnet_names.get(
-                    sn.neutron_subnet_id) or sn.neutron_subnet_id or "-"
+            if base.is_service_enabled(self.request, 'network'):
+                neutron_net_names = dict((net.id, net.name) for net in
+                                         neutron.network_list(self.request))
+                neutron_subnet_names = dict((net.id, net.name) for net in
+                                            neutron.subnet_list(self.request))
+                for sn in share_networks:
+                    sn.neutron_net = neutron_net_names.get(
+                        sn.neutron_net_id) or sn.neutron_net_id or "-"
+                    sn.neutron_subnet = neutron_subnet_names.get(
+                        sn.neutron_subnet_id) or sn.neutron_subnet_id or "-"
+            else:
+                nova_net_names = dict(
+                    [(net.id, net.label)
+                     for net in network.network_list(self.request)])
+                for sn in share_networks:
+                    sn.nova_net = nova_net_names.get(
+                        sn.nova_net_id) or sn.nova_net_id or "-"
         except Exception:
             share_networks = []
             exceptions.handle(self.request,
