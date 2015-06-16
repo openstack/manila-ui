9a4769dbb0c9e18d49a064acdf8df99dd0727f96
Thomas Bechtold <tbechtold@suse.com>
2015-06-16 16:05:32 +0200

    Use UPDATE_HORIZON_CONFIG for manila_ui.overrides
    Instead of manually editing local_settings.py, use the
    UPDATE_HORIZON_CONFIG mechanism to set manila_ui overrides.
    This simplifies the deployment process of the plugin.
    
    Change-Id: I28b74892f6b400342b66cff8b599a29bf6cc6b56

--- manila_ui/enabled/_90_manila_admin_shares.py
+++ manila_ui/enabled/_90_manila_admin_shares.py
@@ -2,3 +2,6 @@ PANEL_DASHBOARD = 'admin'
 PANEL_GROUP = 'admin'
 PANEL = 'shares'
 ADD_PANEL = 'manila_ui.dashboards.admin.shares.panel.Shares'
+UPDATE_HORIZON_CONFIG = {
+    'customization_module': 'manila_ui.overrides',
+}
--- manila_ui/enabled/_90_manila_project_shares.py
+++ manila_ui/enabled/_90_manila_project_shares.py
@@ -4,3 +4,6 @@ PANEL = 'shares'
 ADD_PANEL = 'manila_ui.dashboards.project.shares.panel.Shares'
 # ADD_INSTALLED_APPS enables using html templates from within the plugin
 ADD_INSTALLED_APPS = ['manila_ui.dashboards.project']
+UPDATE_HORIZON_CONFIG = {
+    'customization_module': 'manila_ui.overrides',
+}
