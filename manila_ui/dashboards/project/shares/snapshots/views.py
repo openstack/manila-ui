bd09a423043a615caa9bbd90f40d8b88e140bbb8
Tatiana Ovchinnikova <t.v.ovtchinnikova@gmail.com>
2016-07-26 16:56:36 +0300

    Remove Cinder leftovers from Create Snapshot View
    Share state cannot be "in-use", so the whole if statement
    is unused. This patch removes it and makes a corresponding
    cleanup in html template and unit test.
    
    Change-Id: I5030ed7751e561db7d1b7ab1255d6fa8daf387f9

--- manila_ui/dashboards/project/shares/snapshots/views.py
+++ manila_ui/dashboards/project/shares/snapshots/views.py
@@ -69,6 +69,7 @@ class CreateSnapshotView(forms.ModalFormView):
     template_name = 'project/shares/snapshots/create_snapshot.html'
     modal_header = _("Create Share Snapshot")
     modal_id = "create_share_snapshot_modal"
+    submit_label = _("Create Share Snapshot")
     submit_url = "horizon:project:shares:create_snapshot"
     success_url = reverse_lazy('horizon:project:shares:snapshots_tab')
     page_title = _('Create Share Snapshot')
@@ -79,17 +80,9 @@ class CreateSnapshotView(forms.ModalFormView):
         args = (context['share_id'],)
         context['submit_url'] = reverse(self.submit_url, args=args)
         try:
-            share = manila.share_get(self.request, context['share_id'])
-            if (share.status == 'in-use'):
-                context['attached'] = True
-                context['form'].set_warning(
-                    _("This share is currently attached to an instance. "
-                      "In some cases, creating a snapshot from an attached "
-                      "share can result in a corrupted snapshot."))
             context['usages'] = quotas.tenant_limit_usages(self.request)
         except Exception:
-            exceptions.handle(self.request,
-                              _('Unable to retrieve share information.'))
+            exceptions.handle(self.request, _('Unable to retrieve quotas.'))
         return context
 
     def get_initial(self):
