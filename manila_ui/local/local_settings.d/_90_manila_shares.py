7b3267b2bae436e0580e2a229a64bd8d6a04bc1f
Takashi Kajinami <tkajinam@redhat.com>
2021-02-13 22:55:15 +0900

    Define the default policy file
    This change ensures that the default policy file for Manila API access
    is defined by default, so that operators can deploy their own policy
    more easily.
    
    Change-Id: Ie890766ea2a274791393304cdfe532e024171195

--- manila_ui/local/local_settings.d/_90_manila_shares.py
+++ manila_ui/local/local_settings.d/_90_manila_shares.py
@@ -11,6 +11,12 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from django.conf import settings
+
+
+settings.POLICY_FILES.update({
+    'share': 'manila_policy.json',
+})
 
 # The OPENSTACK_MANILA_FEATURES settings can be used to enable or disable
 # the UI for the various services provided by Manila.
