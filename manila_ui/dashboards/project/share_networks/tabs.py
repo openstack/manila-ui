70940a6917370b705223556a720875dfdc3952eb
Rose Kimondo <rosewairimu1@gmail.com>
2026-03-11 16:15:04 +0300

    Follow up on share network subnets metadata feature
    - This is a follow up patch for the share_network subnets
    - metadata manipulation changes merged in this patch:
    - https://review.opendev.org/c/openstack/manila-ui/+/973509
    - Updates include removing dead amd unused code and optimizing
    - filtering.
    
    Change-Id: I8357902563e9f9ee85d5aebfa7da4f1fb15d4f25
    Signed-off-by: Rose Kimondo <rosewairimu1@gmail.com>

--- manila_ui/dashboards/project/share_networks/tabs.py
+++ manila_ui/dashboards/project/share_networks/tabs.py
@@ -14,10 +14,7 @@
 from django.utils.translation import gettext_lazy as _
 from horizon import exceptions
 from horizon import tabs
-from openstack_dashboard.api import base
-from openstack_dashboard.api import neutron
 
-from manila_ui.api import manila
 from manila_ui.dashboards.project.share_networks import tables as sn_tables
 
 
@@ -27,23 +24,8 @@ class OverviewTab(tabs.Tab):
     template_name = "project/share_networks/_detail.html"
 
     def get_context_data(self, request):
-        share_network = self.tab_group.kwargs['share_network']
-        subnets_to_show = share_network.share_network_subnets
-        filter_string = request.GET.get('metadata_filter', '').strip().lower()
-
-        if filter_string:
-            filtered = []
-            for subnet in subnets_to_show:
-                meta = subnet.get('metadata', {}) or {}
-                meta_str = " ".join(
-                    ["%s=%s" % (k, v) for k, v in meta.items()]).lower()
-                if filter_string in meta_str:
-                    filtered.append(subnet)
-            subnets_to_show = filtered
-
         return {
-            "share_network": share_network,
-            "subnets_to_show": subnets_to_show
+            "share_network": self.tab_group.kwargs['share_network'],
         }
 
 
@@ -57,39 +39,19 @@ class SubnetsTab(tabs.TableTab):
     def get_subnets_data(self):
         try:
             share_network = self.tab_group.kwargs['share_network']
-            request = self.request
             all_subnets = getattr(share_network, 'share_network_subnets', [])
-            if base.is_service_enabled(request, 'network'):
-                for subnet in all_subnets:
-                    try:
-                        subnet["neutron_net"] = neutron.network_get(
-                            request, subnet["neutron_net_id"]).name_or_id
-                    except Exception:
-                        subnet["neutron_net"] = _("Unknown")
-                    try:
-                        subnet["neutron_subnet"] = neutron.subnet_get(
-                            request, subnet["neutron_subnet_id"]).name_or_id
-                    except Exception:
-                        subnet["neutron_subnet"] = _("Unknown")
-            try:
-                azs = manila.availability_zone_list(request)
-                az_list = ", ".join([az.name for az in azs])
-            except Exception:
-                az_list = ""
-            for subnet in all_subnets:
-                if subnet.get("availability_zone") is None:
-                    subnet["availability_zone"] = az_list
             self._tables[
                 self.table_classes[0].Meta.name].kwargs[
                     'share_network_id'] = share_network.id
             filter_string = self.request.GET.get(
-                'subnets__filter__q', '').strip().lower()
+                'subnets_tab__filter__q', '').strip().lower()
             if filter_string:
                 return [
                     subnet for subnet in all_subnets
                     if filter_string in " ".join(
-                        [f"{k}={v}" for k, v in subnet.get(
-                            'metadata', {}).items()]).lower()
+                        [f"{k}={v}" for k, v in (
+                            subnet.get('metadata') or {}).items()]
+                    ).lower()
                 ]
             return all_subnets
         except Exception:
